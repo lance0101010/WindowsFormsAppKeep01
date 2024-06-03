4429ba83275d45af9540a0711375354d7725db7d
lance0101010 <lancekkrcop@gmail.com>
2024-06-03 11:09:55 +0800

    06/03
    加入儲存功能

--- Form1.cs
+++ Form1.cs
@@ -69,7 +69,47 @@ namespace WindowsFormsAppKeep01
 
         private void button2_Click(object sender, EventArgs e)//儲存
         {
+            // 設置對話方塊標題
+            saveFileDialog1.Title = "儲存檔案";
+            // 設置對話方塊篩選器，限制使用者只能選擇特定類型的檔案
+            saveFileDialog1.Filter = "文字檔案 (*.txt)|*.txt|所有檔案 (*.*)|*.*";
+            // 如果希望預設儲存的檔案類型是文字檔案，可以這樣設置
+            saveFileDialog1.FilterIndex = 1;
+            // 如果希望對話方塊在開啟時顯示的初始目錄，可以設置 InitialDirectory
+            saveFileDialog1.InitialDirectory = "C:\\";// 顯示對話方塊，並等待使用者指定儲存的檔案
+            DialogResult result = saveFileDialog1.ShowDialog();
+            FileStream fileStream = null;
+            // 檢查使用者是否選擇了檔案
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    // 使用者指定的儲存檔案的路徑
+                    string saveFileName = saveFileDialog1.FileName;
 
+                    // 使用 FileStream 建立檔案，如果檔案已存在則覆寫
+                    fileStream = new FileStream(saveFileName, FileMode.Create, FileAccess.Write);
+                    // 將 RichTextBox 中的文字寫入檔案中
+                    byte[] data = Encoding.UTF8.GetBytes(richTextBox1.Text);
+                    fileStream.Write(data, 0, data.Length);
+
+                    MessageBox.Show("檔案儲存成功。", "訊息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // 如果發生錯誤，用 MessageBox 顯示錯誤訊息
+                    MessageBox.Show("儲存檔案時發生錯誤: " + ex.Message, "錯誤訊息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    // 關閉資源
+                    fileStream.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("使用者取消了儲存檔案操作。", "訊息");
+            }
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
